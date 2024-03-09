4e4feffef0fa45318b518ca196d987e5ec47d7f9
David Johns <djjohns42@gmail.com>
2024-03-08 22:08:44 -0600

    Update calculate_volts.py to handle forTypeErrors.

--- utils/calculate_volts.py
+++ utils/calculate_volts.py
@@ -4,14 +4,19 @@ import math
 def calculate_volts(
     amps: float = None, ohms: float = None, watts: float = None
 ) -> float:
-
     if amps and ohms:
-        voltage = amps * ohms
+        if not all(isinstance(arg, (int, float)) for arg in (amps, ohms)):
+            raise TypeError("All arguments must be numbers")
+        volts = amps * ohms
 
     if amps and watts:
-        voltage = watts / ohms
+        if not all(isinstance(arg, (int, float)) for arg in (amps, watts)):
+            raise TypeError("All arguments must be numbers")
+        volts = watts / amps
 
     if watts and ohms:
-        voltage = math.sqrt(watts * ohms)
+        if not all(isinstance(arg, (int, float)) for arg in (watts, ohms)):
+            raise TypeError("All arguments must be numbers")
+        volts = math.sqrt(watts * ohms)
 
-    return voltage
+    return volts
